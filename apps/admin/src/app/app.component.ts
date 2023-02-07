caaef2ff90c1bcdfa656453d2f1d902ce385a3cd
Shamnad-s <75739648+Shamnad-s@users.noreply.github.com>
2023-02-07 09:40:39 +0530

    master commit

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import Quill from 'quill';
 
 @Component({
     selector: 'angular-main-project-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    providers: [MessageService, Quill]
+    providers: [MessageService]
 })
 export class AppComponent {
     title = 'admin';
